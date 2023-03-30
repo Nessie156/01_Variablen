ad0a7f7e5aaee51d30cc933824031ee99a269c54
Peter <schmidt.multimedia@gmail.com>
2023-03-30 11:07:46 +0200

    Wertzuweisung mit prompt()

--- script_01.js
+++ script_01.js
@@ -14,15 +14,24 @@
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
 
-let familyName;
-familyName = "Mütze";
-console.log(familyName);
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
 
 /***** 03 Deklaration + Wertzuweisung II *******/
+
+let firstName, familyName;
+
+firstName = prompt("Bitte Vornamen eingeben:");
+familyName = prompt("Bitte Nachnamen eingeben:");
+
+console.log(firstName + " " + familyName);
+
 
 
 
