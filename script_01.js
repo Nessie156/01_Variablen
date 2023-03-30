c4f700991e7ee55ca6ed599e67e04f6741782c7b
Peter <schmidt.multimedia@gmail.com>
2023-03-30 11:53:35 +0200

    03 Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -25,18 +25,25 @@
 
 /***** 03 Deklaration + Wertzuweisung II *******/
 
-let firstName, familyName;
+// let firstName, familyName;
 
-firstName = prompt("Bitte Vornamen eingeben:");
-familyName = prompt("Bitte Nachnamen eingeben:");
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
 
-console.log(firstName + " " + familyName);
+// console.log(firstName + " " + familyName);
 
+// console.log(typeof firstName);
 
+/* JS ist eine untypisierte Sprache! | untyped */
 
+let test;
+test = "Max";
+test = 2;
+test = true;
+test = false;
 
-
-
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
 
 /***** 04 Beispiel - Berechnung *******/
 
