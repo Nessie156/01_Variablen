3b2fbcf162c8f9d13d922045b069bfb65950836e
Peter <schmidt.multimedia@gmail.com>
2023-03-30 12:50:05 +0200

    04 Beispiel - Berechnung

--- script_01.js
+++ script_01.js
@@ -47,9 +47,27 @@
 
 /***** 03a Konstanten *******/
 
-const test = "Max"; // Variable mit KONSTANTEM Inhalt
-test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
-console.log("Inhalt: " + test); // Ausgabe
+// const test = "Max"; // Variable mit KONSTANTEM Inhalt
+// test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
+// console.log("Inhalt: " + test); // Ausgabe
 
 /***** 04 Beispiel - Berechnung *******/
+
+// Deklaration
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+let ageJohn, ageMark;
+
+let date = new Date();
+// console.log(date.getFullYear());
+let year = date.getFullYear();
+
+ageJohn = year - birthYearJohn; // 2023 - 2000
+ageMark = year - birthYearMark; // 2023 - 1990
+
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
+
+
 
