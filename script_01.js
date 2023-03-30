ec96e11c248f2c39005498bcfc49f0d6c25d7553
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-03-30 08:46:06 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/***** Variablen 01 *******/
+
+/** 
+ * 
+ *  Block-Kommentar
+ * 
+*/
+
+
+
+/***** 02 Deklaration + Wertzuweisung I *******/
+
+
+
+
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+
+
+
+
+
+/***** 04 Beispiel - Berechnung *******/
+
